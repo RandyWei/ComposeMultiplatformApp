aa7b161b576fbf31e5ddca94b089d47ddd1c8f51
RandyWei <weizhengheng@chinahrt.com>
2023-07-17 13:18:10 +0800

    删除无用引用

--- shared/src/commonMain/kotlin/ui/screen/HomeScreen.kt
+++ shared/src/commonMain/kotlin/ui/screen/HomeScreen.kt
@@ -5,16 +5,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
